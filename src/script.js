2449b4857bd283d95103c9c53ea592ad9a248c07
Alberto Cavalcante <232014610@aluno.unb.br>
2025-11-08 01:24:20 -0300

    Atualizar projeto com novo código

--- src/script.js
+++ src/script.js
@@ -1,23 +1,67 @@
 //função de mudar imagem pelo id e pela url
-function changeImage(id, url) {
+function mudarImagem(id, url) {
   document.getElementById(id).src = url;
 }
 //função de mudar texto pelo id e pelo texto
-function changeText(id, text) {
-  document.getElementById(id).innerText = text;
+function mudarTexto(id, texto) {
+  document.getElementById(id).innerText = texto;
 }
 
-// Daqui para baixo voce ira escrever
-// o código para resolver o desafio
+let listaPokemon = [];
+let indicePokemon = 0;
 
-function previousPokemon() {
-  alert("Pokemon Anterior");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Anterior");
+async function carregarPokemonsAPI() {
+  const url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1292";
+  
+  const resposta = await fetch(url);
+  const dados = await resposta.json();
+  
+  listaPokemon = dados.results;
+  
+  mostrarPokemonAtual();
 }
 
-function nextPokemon() {
-  alert("Pokemon Seguinte");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Seguinte");
+async function obterDetalhePokemon(nome) {
+  const url = "https://pokeapi.co/api/v2/pokemon/" + nome;
+  
+  const resposta = await fetch(url);
+  const dados = await resposta.json();
+  
+  return dados;
 }
+
+async function mostrarPokemonAtual() {
+  const pokemonAtual = listaPokemon[indicePokemon];
+  const detalhe = await obterDetalhePokemon(pokemonAtual.name);
+  
+  let imagemUrl = detalhe.sprites.front_default;
+  
+  if (!imagemUrl) {
+    imagemUrl = "../assets/missingno.png";
+  }
+  
+  mudarTexto("nome", pokemonAtual.name);
+  mudarImagem("imagemPokemon", imagemUrl);
+}
+
+function pokemonAnterior() {
+  indicePokemon = indicePokemon - 1;
+  
+  if (indicePokemon < 0) {
+    indicePokemon = listaPokemon.length - 1;
+  }
+  
+  mostrarPokemonAtual();
+}
+
+function pokemonProximo() {
+  indicePokemon = indicePokemon + 1;
+  
+  if (indicePokemon >= listaPokemon.length) {
+    indicePokemon = 0;
+  }
+  
+  mostrarPokemonAtual();
+}
+
+carregarPokemonsAPI();
